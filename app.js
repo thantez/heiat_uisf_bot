3d76db53374498df972758db80987c9f2148daeb
watchdots <33148504+watchdots@users.noreply.github.com>
2018-03-05 11:23:47 +0330

    When you coding, choose best keyboard

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const telegraf = require('telegraf')
     , extra = require('telegraf/extra')
     , bot = new telegraf(Token)
     , stage = new Stage()
-    , PORT = process.envenv.PORT || 3000;
-    , URL = process.envenv.URL || 'https://mysterious-hollows-52337.herokuapp.com';
+    , PORT = process.env.PORT || 3000;
+    , URL = process.env.URL || 'https://mysterious-hollows-52337.herokuapp.com';
 
 //#region constant messages
 const joinMessage = `برای عضویت در مجموعه فرهنگی **هیئت شهدای گمنام دانشگاه اصفهان**، ابتدا نام خود را بفرستید.`
