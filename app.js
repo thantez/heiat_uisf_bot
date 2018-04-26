c10ce5b8f77dcd6b61d5b5326a838f7797a36d87
watchdots <33148504+watchdots@users.noreply.github.com>
2018-04-26 11:22:56 +0430

    final Update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const telegraf = require('telegraf')
     , URL = process.env.URL || 'https://mysterious-hollows-52337.herokuapp.com';
 
 //#region constant messages
-const joinMessage = `برای عضویت در مجموعه فرهنگی **هیئت دانشجویی شهدای گمنام دانشگاه اصفهان**، ابتدا نام خود را بفرستید.`
+const joinMessage = `برای عضویت در **هیئت دانشجویی شهدای گمنام دانشگاه اصفهان**، ابتدا نام خود را بفرستید.`
     , startMessage = `سلام. خوش آمدید.
     برای پر کردن فرم عضویت در هیئت، /join را ارسال فرمایید.
     برای بیان انتقادات و پیشنهادات، /contact را ارسال فرمایید.
@@ -32,7 +32,7 @@ bot.startWebhook(`/bot${TOKEN}`, null, PORT)
 //#region bot setting
 bot.use(session())
 bot.use(stage.middleware())
-bot.start(({reply})=>{
+bot.start(({ reply }) => {
     return reply(startMessage);
 })
 bot.catch((err) => {
@@ -41,7 +41,7 @@ bot.catch((err) => {
 //#endregion bot setting
 
 //#region functions
-function done(mem){
+function done(mem) {
     return `نام : ${mem.name}
     نام خانوادگی : ${mem.family}
     زمینه‌ی تحصیلی : ${mem.teach} ترم ${mem.term}
@@ -55,81 +55,81 @@ const join = new Scene('join');
 stage.register(join)
 bot.command('join', (ctx) => ctx.scene.enter('join'))
 join.enter((ctx) => ctx.replyWithMarkdown(joinMessage));
-join.on('text',(ctx)=>{
+join.on('text', (ctx) => {
     let session = ctx.session, message = ctx.message, reply = ctx.reply, from = ctx.from;
-    if(message.text == '/cancel'){
+    if (message.text == '/cancel') {
         ctx.scene.leave();
         return reply(stopMessage);
     }
-    if(message.entities){
+    if (message.entities) {
         return reply(`لطفا محتوای غیر مرتبط (هشتگ،دستور،لینک و...) ارسال نکنید. برای توقف دستور /cancel را ارسال فرمایید.`);
     }
-    if(!session.mem){
-        session.mem={};
+    if (!session.mem) {
+        session.mem = {};
     }
     let mem = session.mem;
-    if(!mem.name){
+    if (!mem.name) {
         mem.name = message.text;
         return reply(`لطفا نام خانوادگی خود را ارسال کنید.`);
     }
-    if(!mem.family){
+    if (!mem.family) {
         mem.family = message.text;
         return reply(`لطفا رشته تحصیلی خود را ارسال کنید.`);
     }
-    if(!mem.teach){
+    if (!mem.teach) {
         mem.teach = message.text;
         return reply(`مشغول به تحصیل در چه ترمی هستید؟`);
     }
-    if(!mem.term){
+    if (!mem.term) {
         let term = parseInt(message.text);
-        if(isNaN(term))
+        if (isNaN(term))
             return reply('لطفا یک عدد بفرستید.');
         mem.term = term;
-        if(from.username == undefined){
+        if (from.username == undefined) {
             return reply(`لطفا با کمک دکمه های زیر شماره همراه خود را به اشتراک بگذارید.(شما ID تلگرام برای تماس ندارید)`,
-            extra.markup((markup) => {
-                return markup
-                  .keyboard([
-                    markup.contactRequestButton('اشتراک شماره همراه'),
-                    'شماره همراه خود را به اشتراک نمیگذارم.'
-                  ])
-              }))
+                extra.markup((markup) => {
+                    return markup
+                        .keyboard([
+                            markup.contactRequestButton('اشتراک شماره همراه'),
+                            'شماره همراه خود را به اشتراک نمیگذارم.'
+                        ])
+                }))
         }
-        else{
+        else {
             return reply(`لطفا با کمک دکمه های زیر اطلاعات تماس خود را به اشتراک بگذارید.`,
-            extra.markup((markup) => {
-                return markup
-                  .keyboard([
-                    markup.contactRequestButton('اشتراک شماره همراه'),
-                    'شماره همراه خود را به اشتراک نمیگذارم.',
-                    'اشتراک ID.'
-                  ])
-              }))
+                extra.markup((markup) => {
+                    return markup
+                        .keyboard([
+                            markup.contactRequestButton('اشتراک شماره همراه'),
+                            'شماره همراه خود را به اشتراک نمیگذارم.',
+                            'اشتراک ID.'
+                        ])
+                }))
         }
     }
-    if(!mem.contact){
-        if(message.text == 'اشتراک ID.'){
-            mem.contact = '@'+from.username;
+    if (!mem.contact) {
+        if (message.text == 'اشتراک ID.') {
+            mem.contact = '@' + from.username;
         }
-        else{
+        else {
             mem.contact = 'کاربر تمایلی به اشتراک اطلاعات تماس خود نداشت.'
         }
-        return reply(` زمینه فعالیتی خود را بنویسید (پشتیبانی،گرافیست،ساخت کلیپ و ...)`,extra.markup(markup.removeKeyboard()));
+        return reply(` زمینه فعالیتی خود را بنویسید (پشتیبانی،گرافیست،ساخت کلیپ و ...)`, extra.markup(markup.removeKeyboard()));
     }
-    if(!mem.work){
+    if (!mem.work) {
         mem.work = message.text;
-        bot.telegram.sendMessage(adminID,done(mem));
+        bot.telegram.sendMessage(adminID, done(mem));
         session.mem = {};
-        if(ctx.scene.leave('join'))
-        return reply(`ممنون، اطلاعات شما برای ما ارسال شد، دراسرع وقت با شما تماس گرفته خواهد شد!`);
+        if (ctx.scene.leave('join'))
+            return reply(`ممنون، اطلاعات شما برای ما ارسال شد، دراسرع وقت با شما تماس گرفته خواهد شد!`);
     }
 });
-join.on('message', ({message,session,reply}) => {
-    if(message.contact){
+join.on('message', ({ message, session, reply }) => {
+    if (message.contact) {
         session.mem.contact = message.contact.phone_number;
-        return reply(` زمینه فعالیتی خود را بنویسید (پشتیبانی،گرافیست،ساخت کلیپ و ...)`,extra.markup(markup.removeKeyboard()));
+        return reply(` زمینه فعالیتی خود را بنویسید (پشتیبانی،گرافیست،ساخت کلیپ و ...)`, extra.markup(markup.removeKeyboard()));
     }
-    else{
+    else {
         reply(`لطفا فایل ارسال نکنید.`)
     }
 });
@@ -138,15 +138,15 @@ join.on('message', ({message,session,reply}) => {
 //#region contact scene setting
 const contact = new Scene('contact');
 stage.register(contact);
-bot.command('contact',(ctx) => ctx.scene.enter('contact'))
-contact.enter(({reply}) => reply(`پیام خود را وارد کنید.`));
-contact.command('cancel',(ctx)=>{
+bot.command('contact', (ctx) => ctx.scene.enter('contact'))
+contact.enter(({ reply }) => reply(`پیام خود را وارد کنید.`));
+contact.command('cancel', (ctx) => {
     ctx.scene.leave();
     return ctx.reply(stopMessage)
 });
-contact.on('message',(ctx)=>{
+contact.on('message', (ctx) => {
     let message = ctx.message;
-    bot.telegram.forwardMessage(adminID,message.from.id,message.message_id);
+    bot.telegram.forwardMessage(adminID, message.from.id, message.message_id);
     ctx.scene.leave('contact');
     return ctx.reply('پیام فرستاده شد.')
 })
